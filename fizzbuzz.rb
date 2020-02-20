0c38475ed72e5b7af9da2835e80cc763327679f4
Marcelo Martinez <marcelomst1@gmail.com>
2020-02-20 12:57:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
-require 'pry'
+# require 'pry'
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  binding.pry
+  # binding.pry
   if (int % 3) == 0 && (int % 5) == 0
     return "FizzBuzz"
   elsif (int % 3) == 0
