4a57e51ac29c144f968a3f1137fa50b46a8e4a34
Marcelo Martinez <marcelomst1@gmail.com>
2020-02-20 12:25:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,10 +5,7 @@ def fizzbuzz(int)
   binding.pry
   if (int % 3) == 0 && (int % 5) == 0
     return "FizzBuzz"
-  elsif (int % 3) == 0
-    return "Fizz"
-  elsif (int % 5) == 0
-    return "Buzz"
+ exit
   else
     return nil
   end 
